fc0153bd59263799932c2f2238c0a5c9e6d02958
dahnev <60144273+DAHNEEV@users.noreply.github.com>
2026-08-16 19:43:08 +0200

    fix: remove old export
    Signed-off-by: dahnev <60144273+DAHNEEV@users.noreply.github.com>

--- crates/modules/authn/src/lib.rs
+++ crates/modules/authn/src/lib.rs
@@ -5,7 +5,6 @@ mod infrastructure;
 pub use application::service::AuthNService;
 pub use application::service::log_in::{LogInCommand, log_in};
 pub use application::service::sign_up::{SignUpCommand, sign_up};
-pub use domain::sessions::config::SessionConfig;
 pub use infrastructure::repositories::{
   account::AccountRepository, session::SessionRepository,
 };
